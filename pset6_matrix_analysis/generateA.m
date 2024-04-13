1ae174ca44d8004522d8d4d1205c15dc760840f8
noamschuck <noam.schuck@gmail.com>
2024-04-13 17:18:52 -0400

    added A matrix generation

---
+++ pset6_matrix_analysis/generateA.m
@@ -0,0 +1,11 @@
+function A = generateA(M, N, thetas, const, PdB, PndB)
+    S = exp(-j*2*pi*const*(0:M-1)'.*cos(thetas+zeros(M,1)))/sqrt(M); % Steering Matrix
+    [tmp, L] = size(thetas);
+
+    varS = (ones([L,1])*10).^(PdB/10) + zeros(1,N);
+    B = sqrt(varS).*(randn(L, N)+j*randn(L, N))/sqrt(2);
+    
+    varN = (ones(M,N)*10).^(PndB/10) + zeros(1, N);
+    V = sqrt(varN).*(randn(M, N)+j*randn(M, N))/sqrt(2);
+    A = S*B + V/sqrt(M);
+end
